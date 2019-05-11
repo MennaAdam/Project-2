1defd6676269e74c1f3060fd3c0953536ce357e0
mennaadam <mennatallah89@hotmail.com>
2019-05-11 16:37:40 +0200

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,59 @@
+
+$(document).ready(function() {
+
+//Adjust Header Height
+
+	$('.main').height($(window).height());
+// Navbar
+	$(window).scroll(function() {
+		var scroll = $(window).scrollTop();
+		if (scroll >= 200) {
+			$(".change").css({
+				"background": "white",
+				"boxShadow": "0px -3px 10px",
+				"position": 'fixed'
+			});
+			$(".nav-link").css("color", "#9e9e9e");
+			$(".navbar-brand").css("color", "black");
+		} else {
+			$(".change").css({
+				"background": "transparent",
+				"boxShadow": "none"
+			});
+			$(".nav-link").css("color", "white");
+			$(".navbar-brand").css("color", "white");
+		}
+	});
+
+	//Smoth Scroll
+
+	$(".nav-link").on('click', function(e) {
+		e.preventDefault();
+		$("html, body").animate({
+			scrollTop: $("#" + $(this).data("scroll")).offset().top + 1
+		}, 500);
+	});
+
+
+	// Open lightbox
+	$('.lightbox-container').on('click', function(e) {
+		e.preventDefault();
+		var imageUrl = $(this).attr('href');
+		var img = $('<img>');
+		$('html').addClass('no-scroll');
+		$('.lightbox-opend').css('display', 'block').append(img);
+		$(img).attr('src', imageUrl);
+		$('.change').css('position', 'relative');
+	});
+
+	// Close lightbox
+	$('#close').on('click', function(e) {
+		e.preventDefault();
+		var img = $('<img>');
+		$('.lightbox-opend img').remove();
+		$('.lightbox-opend').css('display', 'none');
+		$('html').removeClass('no-scroll');
+		$('.change').css('position', 'fixed');
+	});
+
+});
